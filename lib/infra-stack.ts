ce91bfffbb830f7ea2c5f711ffdd189a5576cf0e
rajwinder <rajwinder.kaurmarwaha@educationhorizons.com>
2024-06-11 15:59:23 +1000

    added code for cloudwatch alarm for lambda

--- lib/infra-stack.ts
+++ lib/infra-stack.ts
@@ -1,16 +1,29 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as cloudwatch from 'aws-cdk-lib/aws-cloudwatch';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // create lambda function
+    const demolambda = new lambda.Function(this, 'demolambda', {
+      runtime: lambda.Runtime.PYTHON_3_12,
+      code: lambda.Code.fromAsset('../services/'),
+      handler: 'lambda_function.lambda_handler',
+      functionName: 'demolambda'
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+    // create cloudwatch alarm 
+    
+    const errorAlarm = new cloudwatch.Alarm(this, 'cloudwatchalarm', {
+      metric: demolambda.metricErrors(),
+      threshold: 1,
+      evaluationPeriods: 1,
+      alarmName: 'demolambda-errorAlarm'
+    });
+    
+}
 }
